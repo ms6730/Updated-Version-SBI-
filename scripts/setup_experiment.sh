da600a54d6f2ef7b4cebc2d04884d6e12e418e36
Marie Joe <ms6730@princeton.edu>
2025-01-21 10:51:58 -0500

    adding setup_experiment.sh

---
+++ scripts/setup_experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=sbp_bl
+#SBATCH --output=/home/at8471/c2_sbi_experiments/sbi_framework/%x_%j.out
+#SBATCH --error=/home/at8471/c2_sbi_experiments/sbi_framework/%x_%j.err
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=16      # total number of tasks per node
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=06:00:00
+
+json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/new_press.json'
+#json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/settings.json'
+
+# Set up and do baseline run
+python3 setup_experiment.py "$json_path"
