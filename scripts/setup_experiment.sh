8d04048b27e658107a16964be944e289cc32529c
Marie Joe <ms6730@princeton.edu>
2025-01-21 10:55:33 -0500

    send email when job begins

--- scripts/setup_experiment.sh
+++ scripts/setup_experiment.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=06:00:00
-
+#SBATCH --mail-type=begin        # send email when job begins
 json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/new_press.json'
 #json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/settings.json'
 
