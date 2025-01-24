2d786053b524f3391a0ea847e243994de8c51bfc
Marie Joe <ms6730@princeton.edu>
2025-01-24 13:22:45 -0500

    mdofications on setup_experiment.sh

--- c2_sbi_experiments/hydrogen-sbi/scripts/setup_experiment.sh
+++ c2_sbi_experiments/hydrogen-sbi/scripts/setup_experiment.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=ensemble
+#SBATCH --job-name=sbp_bl
 #SBATCH --output=/home/at8471/c2_sbi_experiments/sbi_framework/%x_%j.out
 #SBATCH --error=/home/at8471/c2_sbi_experiments/sbi_framework/%x_%j.err
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks=1     # total number of tasks per node
+#SBATCH --ntasks=16      # total number of tasks per node
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
-#SBATCH --time=01:00:00
-#SBATCH --mail-type=begin        # send email when job begins
-#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --time=06:00:00
 
-json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/settings.json' 
+json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/new_press.json'
+#json_path='/home/at8471/c2_sbi_experiments/hydrogen-sbi/scripts/settings.json'
 
 # Set up and do baseline run
-module purge                     # machine learning purposes 
-module load anaconda3/2021.11    # to manage software packages and environments 
-conda activate sbi_new           # to activate the environment 
 python3 setup_experiment.py "$json_path"
+
