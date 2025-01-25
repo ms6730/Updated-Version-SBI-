3279f2ac1ffb68b9e8993971fd86844cfc8a0584
Marie Joe <ms6730@princeton.edu>
2025-01-25 14:53:46 -0500

    adding a shell script

---
+++ c2_sbi_experiments/hydrogen-sbi/scripts/evaluate_ensemble.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=ensemble
+#SBATCH --output=/home/ms6730/c2_sbi_experiments_new/sbi_framework/%x_%j.out
+#SBATCH --error=/home/ms6730/c2_sbi_experiments_new/sbi_framework/%x_%j.err
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1      # total number of tasks per node
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=01:00:00
+#SBATCH --array=1-100%4
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=ms6730@princeton.edu
+# Calculate the task ID based on the SLURM_ARRAY_TASK_ID
+task_id=$((SLURM_ARRAY_TASK_ID - 1))
+
+#set path to json file with run settings
+json_path='/home/ms6730/c2_experiment_sin/hydrogen-sbi/scripts/settings.json'
+module purge
+module load anaconda3/2021.11
+conda activate sbi_new
+# Run your Python script
+python3 create_ensemble.py "$task_id" "$json_path"
